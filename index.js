201d0564c2505ce600bb0465f370029c81d9784e
kinsmanEnu <kinsman_enu18@yahoo.com>
2025-03-08 10:58:55 +0100

    setting up vidly

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express()
+app.use(express.json());
+
+const movies = [
+    {id: 1, genre: 'Horror'},
+    {id: 2, genre: 'Drama'},
+    {id: 3, genre: 'Western'},
+    {id: 4, genre: 'Romance'},
+    {id: 5, genre: 'Science Friction'},
+    {id: 6, genre: 'Action'},
+    {id: 7, genre: 'Comedy'},
+    {id: 8, genre: 'Thriller'},
+]
